77b0877ef79e1e2d50e9e4faa6996fa875acf632
kriti <kritigirnar@gmail.com>
2022-10-05 19:01:38 +0530

    filter changes'

---
+++ src/main/java/com/in28Minutes/Microservices/apigateway/LoggingFilter.java
@@ -0,0 +1,22 @@
+package com.in28Minutes.Microservices.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+public class LoggingFilter implements GlobalFilter{
+
+	private Logger logger=LoggerFactory.getLogger(LoggingFilter.class);
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+	
+		logger.info("path of the input request",exchange.getRequest().getURI());
+		return chain.filter(exchange);
+	}
+
+	
+}
